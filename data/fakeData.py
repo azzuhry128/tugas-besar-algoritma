4cf2682a0fc3da669cc0d74ddabd3a119a5a007b
muhammad rofif azzuhry <121540410+azzuhry128@users.noreply.github.com>
2023-12-23 18:08:13 +0000

    program struk.py temporer

---
+++ data/fakeData.py
@@ -0,0 +1,21 @@
+pesanan = {
+    "resi": 8374862837,
+    "nama pembeli": 'surya atmajaya',
+    'nomor hp' : '082249034332',
+    'pesanan': [
+        {
+            "nama barang": "cultivator",
+            "harga": 10000000,
+            "kuantitas": 3,
+            "subtotal": 30000000
+        },
+        {
+            "nama barang": "sprayer",
+            "harga": 20000000,
+            "kuantitas": 2,
+            "subtotal": 40000000
+        },
+    ],
+    'total': 70000000
+}
+
--- src/struk.py
+++ src/struk.py
@@ -1,10 +1,46 @@
+pesanan = {
+    "resi": 8374862837,
+    "pembeli": 'surya atmajaya',
+    'hp' : '082249034332',
+    'barang': [
+        {
+            "nama": "cultivator",
+            "harga": 10000000,
+            "kuantitas": 3,
+            "subtotal": 30000000
+        },
+        {
+            "nama": "sprayer",
+            "harga": 20000000,
+            "kuantitas": 2,
+            "subtotal": 40000000
+        },
+    ],
+    'total': 70000000
+}
+
+
+
 def cetakStruk(data):
-    print(f"
-          RESI PEMBELIAN
-          NO RESI       : ${nomor_resi}
-          nama pembeli  : ${nama_pembeli}
-          nomor hp      : ${nomor_hp}
-          ")
-    print(f"
-          tabulate
-          ")
+    resi, pembeli, hp, barang, total = data.values()
+
+    print(f'''
+        RESI PEMBELIAN
+        NO RESI       : {resi}
+        nama pembeli  : {pembeli}
+        nomor hp      : {hp}
+    ''')
+
+    for items in barang:
+        print(f'''
+        nama barang: {items["nama"]}
+        harga barang: {items["harga"]}
+        kuantitas: {items["kuantitas"]}
+        subtotal: {items["subtotal"]}
+        ''')
+
+    print(f'''
+        total: {total}
+    ''')
+
+cetakStruk(pesanan)
