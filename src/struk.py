b744da31d79ed18ea8f2deafb9fb581bac0a0430
muhammad rofif azzuhry <121540410+azzuhry128@users.noreply.github.com>
2023-12-23 17:24:07 +0000

    inisiasi proyek

---
+++ src/struk.py
@@ -0,0 +1,10 @@
+def cetakStruk(data):
+    print(f"
+          RESI PEMBELIAN
+          NO RESI       : ${nomor_resi}
+          nama pembeli  : ${nama_pembeli}
+          nomor hp      : ${nomor_hp}
+          ")
+    print(f"
+          tabulate
+          ")
