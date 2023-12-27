aef5521ceb83c8c34cc1be44ccf0ab3f946ab4e0
Wilyam2005 <154685868+Wilyam2005@users.noreply.github.com>
2023-12-27 22:00:01 +0700

    Update pemilihan.py

---
+++ src/pemilihan.py
@@ -0,0 +1,49 @@
+import mysql.connector
+
+def connectionDatabase():
+    connector = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="",
+                database="algoritma"
+            )
+    if connector.is_connected:
+        print("Database terkoneksi")
+        return connector
+    else:
+        print("Gagal terhubung ke Database")
+        return False
+connectionDatabase()
+
+def piliMenu(nama):
+    try:
+        conn = connectionDatabase()
+        cursor = conn.cursor()
+        sql = "SELECT * FROM barang WHERE nama = %s" 
+        data = (nama,)
+        cursor.execute(sql,data)
+        result = cursor.fetchall()
+        if result:  
+            print(result)
+        else:
+            print("Tidak ada data yang ditemukan")
+    except mysql.connector.Error as error:
+        print("kesalahan : ",error)
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+while True :
+    print("==========PILIHAN MENU==========")
+    print("1.Lihat Harga Barang")
+    print("2.KELUAR")
+    pilihan=input("Masukkan Pilihan Anda[1-2] : ")
+    if pilihan == '1':
+        print("=====BARANG YANG  DI JUAL=====")
+        print("cultivator")
+        print("traktor")
+        print("mesin perontok padi")
+        namaBarang=input("masukkan Nama Barang [cultivator/traktor/mesin perontok padi] : ")
+        piliMenu(namaBarang)
+    elif pilihan == '2':
+        break
