36522363d3019172d354446f691fcde7bf0f3e43
muhammad rofif azzuhry <muhammadazzuhry04@gmail.com>
2024-01-11 09:16:04 +0700

    manufaktur

---
+++ src/manufaktur.py
@@ -0,0 +1,2 @@
+def Manufaktur():
+    print("manufaktur ")
--- src/struk.py
+++ src/struk.py
@@ -1,34 +1,34 @@
-pesanan = {
-    "resi": 8374862837,
-    "pembeli": 'surya atmajaya',
-    'hp' : '082249034332',
-    'barang': [["cultivator", 10000000, 3, 30000000],["sprayer", 20000000, 2, 40000000],],
-    'metode': 'kartu debit',
-    'total': 70000000
-}
-
+import datetime
 from tabulate import tabulate
 
 header = ['nama barang', 'harga satuan', 'kuantitas', 'subtotal']
 
-def cetakStruk(data):
-    resi, pembeli, hp, barang, metode, total = data.values()
+date= datetime.datetime.now()
+formatted_date = date.strftime('%Y-%m-%d')
+miliseconds = int(date.timestamp() * 1000)
+
+pesanan = {
+    "resi": miliseconds,
+    'tanggal': formatted_date,
+    "pembeli": 'surya atmajaya',
+    'hp' : '082249034332',
+    'barang': [{"cultivator", 10000000, 3, 30000000},{"sprayer", 20000000, 2, 40000000},],
+    'metode': 'kartu debit',
+    'total': 70000000
+}
+
+def CetakStruk(data):
+    resi, tanggal,pembeli, hp, barang, metode, total = data.values()
 
     print("\nresi pembelian")
     print("NO. Resi: ", resi)
+    print("tanggal: ", tanggal)
     print("nama pembeli: ", pembeli)
     print("telepon: ", hp)
 
     print(tabulate(barang, headers=header, tablefmt='grid'))
 
-    # for items in barang:
-    #     print(f'''
-    #     nama barang: {items["nama"]}
-    #     harga barang: {items["harga"]}
-    #     kuantitas: {items["kuantitas"]}
-    #     subtotal: {items["subtotal"]}
-    #     ''')
     print('metode pembayaran: ', metode)
     print('total pembayaran: ', total)
 
-cetakStruk(pesanan)
+CetakStruk(pesanan)
